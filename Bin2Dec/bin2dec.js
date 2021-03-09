dc22489919a879c23af824782b2c171ab6be5b49
Vinicius Scarabelli <scarabelli.vinicius@gmail.com>
2021-03-08 23:33:52 -0300

    bin2dec
    o bug persiste ainda

--- Bin2Dec/bin2dec.js
+++ Bin2Dec/bin2dec.js
@@ -1,6 +1,8 @@
 
 //DECIMAL PARA BINARIO
 decNumber = document.getElementById("inputDecimal")
+binNumber = document.getElementById("inputBinario")
+
 decNumber.addEventListener("input", ()=>{
     
     var restStack = [], rest, binaryString = '';
@@ -13,17 +15,16 @@ decNumber.addEventListener("input", ()=>{
     while(restStack.length > 0){
         binaryString += restStack.pop().toString();
     };
-    document.getElementById("inputBinario").value = binaryString;
+    binNumber.value = binaryString;
 });
 
 //BINARIO PARA DECIMAL
-binNumber = document.getElementById("inputBinario")
 binNumber.addEventListener("input", ()=>{
     let decimal = 0;
     for(let i = 0; i < binNumber.value.length ; i++){
         let bit = binNumber.value.charAt(binNumber.value.length -i-1);
         decimal += parseInt(bit) * Math.pow(2, i)
     }
-    document.getElementById("inputDecimal").value = decimal;
+    decNumber.value = decimal;
 });
 
