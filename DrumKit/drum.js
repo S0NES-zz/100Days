342a7e83e4cc8844248c3fe7d00ba35d0c4f6377
S0NES <scarabelli.vinicius@gmail.com>
2021-03-15 21:14:44 -0300

    finish day 8

--- DrumKit/drum.js
+++ DrumKit/drum.js
@@ -1,69 +1,67 @@
 let drums = document.querySelectorAll(".drum");
-const folder = "sounds"
+const folder = "sounds";
 
 drums.forEach((drum) => {
-  drum.addEventListener("click", ()=>{
-      handleAudio(drum, folder);
+  drum.addEventListener("click", () => {
+    handleAudioClick(drum, folder);
+  });
+
+  document.addEventListener("keydown", function (event) {
+    makeSound(event.key, folder);
   });
 });
-function handleAudio(drum, folder) {
-  let audio;
+
+function handleAudioClick(drum, folder) {
   switch (drum.innerText) {
     case "w":
       audio = new Audio(folder + "/tom-1.mp3").play();
       break;
     case "a":
-      audio = new Audio(folder +"/tom-2.mp3").play();
+      audio = new Audio(folder + "/tom-2.mp3").play();
       break;
     case "s":
       audio = new Audio(folder + "/tom-3.mp3").play();
       break;
     case "d":
-      audio = new Audio(folder +"/tom-4.mp3").play();
+      audio = new Audio(folder + "/tom-4.mp3").play();
       break;
     case "j":
-      audio = new Audio(folder +"/kick-bass.mp3").play();
+      audio = new Audio(folder + "/kick-bass.mp3").play();
       break;
     case "k":
-      audio = new Audio(folder +"/snare.mp3").play();
+      audio = new Audio(folder + "/snare.mp3").play();
       break;
-    case "l":
-      audio = new Audio(folder +"/crash.mp3").play();
+    case "L":
+      audio = new Audio(folder + "/crash.mp3").play();
       break;
     default:
       return;
   }
 }
-/*
-let drums = document.querySelectorAll(".drum");
-
-drums.forEach((drum) => {
-  drum.addEventListener("click", () => {
-    let audio;
-    switch (drum.innerText) {
-      case "w":
-        audio = new Audio("sounds/tom-1.mp3").play();
-        break;
-      case "a":
-        audio = new Audio("sounds/tom-2.mp3").play();
-        break;
-      case "s":
-        audio = new Audio("sounds/tom-3.mp3").play();
-        break;
-      case "d":
-        audio = new Audio("sounds/tom-4.mp3").play();
-        break;
-      case "j":
-        audio = new Audio("sounds/kick-bass.mp3").play();
-        break;
-      case "k":
-        audio = new Audio("sounds/snare.mp3").play();
-        break;
-      case "l":
-        audio = new Audio("sounds/crash.mp3").play();
-        break;
-      default:
-        return;
-    }
-  });
-});*/
+function makeSound(key, folder) {
+  switch (key) {
+    case "w":
+      audio = new Audio(folder + "/tom-1.mp3").play();
+      break;
+    case "a":
+      audio = new Audio(folder + "/tom-2.mp3").play();
+      break;
+    case "s":
+      audio = new Audio(folder + "/tom-3.mp3").play();
+      break;
+    case "d":
+      audio = new Audio(folder + "/tom-4.mp3").play();
+      break;
+    case "j":
+      audio = new Audio(folder + "/kick-bass.mp3").play();
+      break;
+    case "k":
+      audio = new Audio(folder + "/snare.mp3").play();
+      break;
+    case "l":
+      audio = new Audio(folder + "/crash.mp3").play();
+      break;
+    default:
+      return;
+  }
+}
---
+++ DrumKit/readme.md
@@ -0,0 +1,25 @@
+# Drum Simulator
+
+**Tier:** 1-Beginner
+
+Drum is a wonderful instrument, used for various musical types 
+
+## User Stories
+
+-   [✔️] User can hear the sound when clicked on the buttons
+-   [✔️] User can hear the sound when type current keys
+-   [✔️] each buttons/keys has differrent audio
+
+## Bonus features
+
+-   [] hidden feature to change audio
+-   [] image move when pressed.
+
+## Useful links and resources
+
+- [How to make a dice game](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwiloqzKp7PvAhWaDrkGHT1DAV8QFjAEegQIARAD&url=https%3A%2F%2Fjavascript.plainenglish.io%2Fbuilding-a-simple-drum-kit-with-vanilla-javascript-e391e2f4e2ab&usg=AOvVaw08yt5P4BypEisFK8x7Wvhr)
+- [MDN](https://developer.mozilla.org/en-US/)
+
+## 6/100 3/15/2021 - DONE
+
+## #100DAYSCHALLENGE
