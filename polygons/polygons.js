47abd19e9e78fd5003d19d86d9dbd81518b39677
S0NES <scarabelli.vinicius@gmail.com>
2021-03-18 17:27:15 -0300

    final changes

--- polygons/polygons.js
+++ polygons/polygons.js
@@ -1,21 +1,18 @@
-let input, inpX, inpY, inpZ, cube;
-
-cube = document.querySelector(".cube");
-inpX = document.getElementById("inputX");
-inpY = document.getElementById("inputY");
-inpZ = document.getElementById("inputZ");
-
+let cube = document.querySelector(".cube");
+let inpX = document.getElementById("inputX");
+let inpY = document.getElementById("inputY");
+let inpZ = document.getElementById("inputZ");
 
 cube.style.transform = "";
 
 inpX.addEventListener("input", () => {
-    cube.style = "transform: rotateX( " + inpX.value + "deg);";
+  cube.style = "transform: rotateX( " + inpX.value + "deg);";
 });
 
 inpY.addEventListener("input", () => {
-    cube.style = "transform: rotateY( " + inpY.value + "deg);";
+  cube.style = "transform: rotateY( " + inpY.value + "deg);";
 });
 
 inpZ.addEventListener("input", () => {
-    cube.style = "transform: rotateZ( " + inpZ.value + "deg);";
+  cube.style = "transform: rotateZ( " + inpZ.value + "deg);";
 });
