13c56d2a682b87abe0d67d797c125b5b4243e52b
S0NES <scarabelli.vinicius@gmail.com>
2021-03-19 17:58:19 -0300

    finalizei dia 12 fii

--- bgCube/bgCube.js
+++ bgCube/bgCube.js
@@ -1,10 +1,33 @@
-file = document.getElementById("inpFile");
+inpFile = document.getElementById("inpFile");
 videos = document.querySelectorAll(".videos");
 
-file.addEventListener("change", e => {
-  videos.forEach(video => {
-    video.src = e.target.defaultValue;
-    console.log(e.target);
-  });
-});
+inpFile.addEventListener("change", handleFile);
 
+function handleFile(e) {
+  const file = e.target.files;
+  const src = URL.createObjectURL(file[0]);
+  if (file !== 0 || file !== undefined) {
+    if (
+      file[0].type === "image/jpeg" ||
+      file[0].type === "image/tiff" ||
+      file[0].type === "image/png" ||
+      file[0].type === "image/gif" ||
+      file[0].type === "image/svg"
+    ) {
+      handleImg(src);
+    } else if (file[0].type === "video/mp4") {
+      handleVideo(src);
+    }
+  }
+}
+function handleImg(src) {
+  videos.forEach((video) => {
+      video.src = "";
+    video.poster = src;
+  });
+}
+function handleVideo(src) {
+  videos.forEach((video) => {
+    video.src = src;
+  });
+}
