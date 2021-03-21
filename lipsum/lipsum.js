df3966aeacffa0e4387f649a26a52090ff5d1af6
S0NES <scarabelli.vinicius@gmail.com>
2021-03-21 12:30:57 -0300

    verdadeiro gerador de lore ipsum

--- lipsum/lipsum.js
+++ lipsum/lipsum.js
@@ -1,4 +1,96 @@
-import { LoremIpsum } from "lorem-ipsum";
+let lipsum = {
+  paragraph: [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacinia quis vel eros donec. Nam at lectus urna duis convallis convallis tellus. Id porta nibh venenatis cras sed felis eget. Enim nec dui nunc mattis. Nulla aliquet enim tortor at auctor urna nunc id. Eu mi bibendum neque egestas congue. Facilisis magna etiam tempor orci eu lobortis. Id semper risus in hendrerit gravida rutrum. Purus semper eget duis at tellus at urna. Viverra nam libero justo laoreet. Tortor posuere ac ut consequat semper viverra nam libero. Auctor neque vitae tempus quam. Risus commodo viverra maecenas accumsan. Diam ut venenatis tellus in metus. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Adipiscing elit pellentesque habitant morbi tristique senectus. At tempor commodo ullamcorper a lacus vestibulum sed arcu non. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam. Erat nam at lectus urna duis convallis convallis tellus id. ",
+    "Nulla pharetra diam sit amet nisl suscipit. Tortor aliquam nulla facilisi cras fermentum. Id diam vel quam elementum pulvinar etiam non quam lacus. Elementum sagittis vitae et leo duis ut diam quam nulla. Tempor orci eu lobortis elementum nibh tellus. Interdum velit euismod in pellentesque. Dictum fusce ut placerat orci nulla pellentesque dignissim. Gravida rutrum quisque non tellus orci ac auctor augue. Natoque penatibus et magnis dis. Nunc sed id semper risus in hendrerit gravida. Ut lectus arcu bibendum at varius vel pharetra vel turpis. Neque ornare aenean euismod elementum. Et egestas quis ipsum suspendisse ultrices. Ut morbi tincidunt augue interdum velit. ",
+    "Arcu vitae elementum curabitur vitae nunc sed velit dignissim. Tristique magna sit amet purus. Nec dui nunc mattis enim ut. Semper eget duis at tellus at urna condimentum mattis pellentesque. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat. Nibh sit amet commodo nulla facilisi nullam vehicula ipsum a. Nunc eget lorem dolor sed viverra ipsum. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Blandit aliquam etiam erat velit scelerisque in dictum non consectetur. Dolor morbi non arcu risus quis. ",
+    "Massa placerat duis ultricies lacus sed turpis tincidunt. Auctor eu augue ut lectus arcu bibendum. Sed vulputate mi sit amet mauris. Nulla pellentesque dignissim enim sit amet venenatis urna cursus eget. Pellentesque adipiscing commodo elit at imperdiet dui. At elementum eu facilisis sed odio morbi quis commodo. Fringilla phasellus faucibus scelerisque eleifend. In nisl nisi scelerisque eu ultrices vitae auctor. Montes nascetur ridiculus mus mauris. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Integer vitae justo eget magna fermentum iaculis eu non diam. Urna nunc id cursus metus. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. Ultrices sagittis orci a scelerisque purus semper eget duis. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Lorem donec massa sapien faucibus et molestie ac feugiat sed. Et tortor at risus viverra adipiscing at. Sed id semper risus in hendrerit gravida. Cursus in hac habitasse platea dictumst. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. ",
+    "Tellus in metus vulputate eu scelerisque felis imperdiet proin. Malesuada bibendum arcu vitae elementum curabitur. Est velit egestas dui id ornare arcu odio. At tellus at urna condimentum mattis pellentesque id nibh. Sed enim ut sem viverra aliquet eget sit. Lobortis elementum nibh tellus molestie nunc non blandit massa. Id volutpat lacus laoreet non curabitur gravida arcu. Pellentesque sit amet porttitor eget dolor. Et netus et malesuada fames ac turpis. Egestas fringilla phasellus faucibus scelerisque eleifend donec. Lacus laoreet non curabitur gravida. Amet volutpat consequat mauris nunc congue. Curabitur gravida arcu ac tortor dignissim convallis. ",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis ultricies lacus sed. Elementum sagittis vitae et leo duis ut diam quam. Arcu non sodales neque sodales ut etiam sit amet nisl. Auctor elit sed vulputate mi sit amet mauris commodo quis. Sapien nec sagittis aliquam malesuada. Potenti nullam ac tortor vitae purus. Magna fermentum iaculis eu non diam. Feugiat sed lectus vestibulum mattis. In ante metus dictum at tempor commodo. Lacus viverra vitae congue eu. Ornare aenean euismod elementum nisi quis eleifend quam. Dolor morbi non arcu risus quis varius quam. A lacus vestibulum sed arcu non odio. Elit scelerisque mauris pellentesque pulvinar. Sapien pellentesque habitant morbi tristique senectus et netus et malesuada. ",
+    "Nibh mauris cursus mattis molestie. In vitae turpis massa sed. Sed vulputate odio ut enim. Vivamus at augue eget arcu dictum varius. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Iaculis eu non diam phasellus vestibulum lorem sed. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum. Eu lobortis elementum nibh tellus molestie nunc non blandit. Vestibulum lectus mauris ultrices eros in cursus turpis massa. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Nisl tincidunt eget nullam non nisi est sit amet facilisis. Nunc congue nisi vitae suscipit tellus mauris. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. Lectus urna duis convallis convallis tellus id. Porttitor eget dolor morbi non arcu risus quis varius quam. Pellentesque pulvinar pellentesque habitant morbi. Vel fringilla est ullamcorper eget nulla facilisi etiam. ",
+    "Mattis nunc sed blandit libero volutpat sed cras ornare. In ante metus dictum at tempor commodo. Nibh venenatis cras sed felis eget velit. Elit ullamcorper dignissim cras tincidunt. Vitae tortor condimentum lacinia quis vel eros donec ac. Diam phasellus vestibulum lorem sed risus. Dolor sit amet consectetur adipiscing elit duis. Tincidunt id aliquet risus feugiat in ante metus. Tellus id interdum velit laoreet id donec ultrices tincidunt. Sed elementum tempus egestas sed. Viverra justo nec ultrices dui sapien eget. Arcu non odio euismod lacinia at. Quam lacus suspendisse faucibus interdum posuere lorem. ",
+    "Enim neque volutpat ac tincidunt vitae semper quis lectus. Sit amet venenatis urna cursus eget nunc. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Sapien pellentesque habitant morbi tristique senectus et netus. Ac turpis egestas sed tempus urna et pharetra. Vitae congue eu consequat ac felis donec et odio. Bibendum neque egestas congue quisque egestas diam in arcu. Sit amet facilisis magna etiam. Cras semper auctor neque vitae. Felis imperdiet proin fermentum leo vel. ",
+    "Habitant morbi tristique senectus et netus. Arcu non odio euismod lacinia at quis risus sed vulputate. Curabitur gravida arcu ac tortor dignissim convallis aenean. Enim ut tellus elementum sagittis vitae et leo. Sit amet nisl suscipit adipiscing. Sollicitudin aliquam ultrices sagittis orci a. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Lacinia at quis risus sed vulputate. Ultrices sagittis orci a scelerisque purus semper eget. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Enim ut sem viverra aliquet. Diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Adipiscing tristique risus nec feugiat. Sem fringilla ut morbi tincidunt augue. Nulla facilisi morbi tempus iaculis urna id volutpat. ",
+  ],
+  sentence: [
+    "Lorem ipsum dolor sit amet  ",
+    "consectetur adipiscing elit.  ",
+    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ",
+    "Mi bibendum neque egestas congue quisque egestas diam in arcu ",
+    "Lorem donec massa sapien faucibus et  ",
+    "Eu lobortis elementum nibh tellus molestie nunc non blandit massa.  ",
+    "Integer feugiat scelerisque varius morbi enim nunc faucibus  ",
+    "Morbi tincidunt ornare massa eget egestas purus viverra accumsan.  ",
+    "Faucibus in ornare quam viverra orci sagittis. Pretium lectus quam id leo in vitae turpis massa ",
+    " Cras semper auctor neque vitae tempus quam pellentesque nec ",
+    "Facilisi cras fermentum odio eu feugiat pretium.  ",
+    "Ac turpis egestas sed tempus. Blandit massa enim nec dui nunc.  ",
+    "Diam maecenas sed enim ut sem viverra aliquet. Viverra nibh cras pulvinar mattis nunc sed. ",
+    "Vestibulum morbi blandit cursus risus at. ",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit ",
+    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    "Convallis convallis tellus id interdum velit laoreet id donec ultrices ",
+    "Enim lobortis scelerisque fermentum dui faucibus in ",
+    "Aliquam etiam erat velit scelerisque in dictum non ",
+    "Morbi tristique senectus et netus et malesuada fames ac turpis  ",
+    "Ut morbi tincidunt augue interdum velit. Pretium aenean pharetra magna ac placerat vestibulum lectus.  ",
+    "tristique senectus et netus et malesuada fames ac turpis. Magna etiam tempor orci eu lobortis elementum nibh tellus  ",
+    "Ac odio tempor orci dapibus ultrices in iaculis nunc.   ",
+    "Feugiat vivamus at augue eget arcu dictum varius duis  ",
+    "Condimentum lacinia quis vel eros. In est ante in nibh mauris cursus  ",
+    "Pretium aenean pharetra magna ac placerat vestibulum lectus.  ",
+    "Ullamcorper morbi tincidunt ornare massa eget egestas purus.  ",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit  ",
+    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus sed viverra tellus in  ",
+    "Varius sit amet mattis vulputate enim nulla aliquet porttitor  ",
+    "Donec pretium vulputate sapien nec sagittis aliquam malesuada  ",
+    "Ut ornare lectus sit amet. Aliquet bibendum enim facilisis gravida  ",
+    "Porttitor lacus luctus accumsan tortor posuere ac ut consequat semper  ",
+    "Purus sit amet volutpat consequat mauris. Justo nec ultrices dui sapien  ",
+    "Commodo elit at imperdiet dui accumsan sit amet  ",
+    "emper risus in hendrerit gravida  ",
+    "Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate  ",
+    "Massa ultricies mi quis hendrerit dolor. Leo vel fringilla est ullamcorper  ",
+    "Tristique senectus et netus et malesuada fames.  ",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit  ",
+  ],
+  words: [
+    "nunc  ",
+    "mi  ",
+    "ipsum  ",
+    "faucibus  ",
+    "vitae  ",
+    "aliquet  ",
+    "nec  ",
+    "ullamcorper  ",
+    "sit  ",
+    "amet  ",
+    "risus  ",
+    "nullam  ",
+    "eget  ",
+    "felis  ",
+    "eget  ",
+    "nunc  ",
+    "lobortis  ",
+    "mattis  ",
+    "aliquam  ",
+    "faucibus  ",
+    "purus  ",
+    "in  ",
+    "massa  ",
+    "tempor  ",
+    "nec  ",
+    "feugiat  ",
+    "nisl  ",
+    "pretium  ",
+    "fusce  ",
+    "id  ",
+    "velit  ",
+    "ut  ",
+    "tortor  ",
+    "pretium  ",
+    "viverra  ",
+  ],
+};
 
 let input = document.getElementById("value");
 let select = document.getElementById("select");
@@ -8,34 +100,38 @@ let copyBtn = document.getElementById("copy");
 
 let generate = document.getElementById("generate");
 
-let text = document.getElementById("generated")
-
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4,
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4,
-  },
-});
+let text = document.getElementById("generated");
 
 select.addEventListener("change", () => {
   selected.value = select.options[select.selectedIndex].value;
 });
 
 generate.addEventListener("click", () => {
-  selected.value = input.value;
-  switch (selected.innerText) {
-    case "Paragraph":
-      text.innerText = lorem.generateParagraphs(selected.value);
+  let aux = "";
+  switch (selected.value) {
+    case "paragraph":
+      text.value = "";
+      for (var i = input.value; i > 0; i--) {
+        aux = lipsum.paragraph.shift();
+        text.value += aux;
+        lipsum.paragraph.push(aux);
+      }
       break;
-    case "Word":
-      text.innerText = lorem.generateWords(selected.value);
+    case "words":
+      text.value = "";
+      for (var i = input.value; i > 0; i--) {
+        aux = lipsum.words.shift();
+        text.value += aux;
+        lipsum.words.push(aux);
+      }
       break;
-    case "Sentence":
-      text.innerText = lorem.generateSentences(selected.value);
+    case "sentence":
+      text.value = "";
+      for (var i = input.value; i > 0; i--) {
+        aux = lipsum.sentence.shift();
+        text.value += aux;
+        lipsum.sentence.push(aux);
+      }
       break;
     default:
       return;
