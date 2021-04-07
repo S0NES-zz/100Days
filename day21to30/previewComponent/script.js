d938875aeae42ad0b132da0c42e05d4bbabdcb17
S0NES <scarabelli.vinicius@gmail.com>
2021-04-07 09:38:26 -0300

    updates on day 29

--- day21to30/previewComponent/script.js
+++ day21to30/previewComponent/script.js
@@ -1,11 +1,17 @@
 let check = document.getElementById("shareCheck");
 let share = document.querySelector(".shareDiv");
+let path = document.getElementById("path");
+let label = document.getElementById("shareLabel");
 
 check.addEventListener("click", () => {
   if (check.checked == 1) {
     share.style.display = "flex";
+    path.style = "fill: var(--lightGrayishBlue);";
+    label.style = "background: var(--desaturatedDarkBlue);";
   }
   if (check.checked == 0) {
     share.style.display = "none";
+    path.style = "fill: var(--desaturatedDarkBlue);";
+    label.style = "background: var(--lightGrayishBlue);";
   }
 });
