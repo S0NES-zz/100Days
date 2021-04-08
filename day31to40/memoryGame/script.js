896156363fd43a04be83e75bab81b9fff4131066
S0NES <scarabelli.vinicius@gmail.com>
2021-04-08 19:56:02 -0300

    day 32

--- day31to40/memoryGame/script.js
+++ day31to40/memoryGame/script.js
@@ -1,157 +1,194 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cardArray = [
-    {
-      name: "controller",
-      img: "img/controller.svg",
-    },
-    {
-      name: "controller",
-      img: "img/controller.svg",
-    },
-    {
-      name: "gift",
-      img: "img/gift.svg",
-    },
-    {
-      name: "gift",
-      img: "img/gift.svg",
-    },
-    {
-      name: "key",
-      img: "img/key.svg",
-    },
-    {
-      name: "key",
-      img: "img/key.svg",
-    },
-    {
-      name: "magnet",
-      img: "img/magnet.svg",
-    },
-    {
-      name: "magnet",
-      img: "img/magnet.svg",
-    },
-    {
-      name: "location",
-      img: "img/location.svg",
-    },
-    {
-      name: "location",
-      img: "img/location.svg",
-    },
-    {
-      name: "magnet",
-      img: "img/magnet.svg",
-    },
-    {
-      name: "magnet",
-      img: "img/magnet.svg",
-    },
-    {
-      name: "seat",
-      img: "img/seat.svg",
-    },
-    {
-      name: "seat",
-      img: "img/seat.svg",
-    },
-    {
-      name: "sun",
-      img: "img/sun.svg",
-    },
-    {
-      name: "sun",
-      img: "img/sun.svg",
-    },
-    {
-      name: "wifi",
-      img: "img/wifi.svg",
-    },
-    {
-      name: "wifi",
-      img: "img/wifi.svg",
-    },
-    {
-      name: "heart",
-      img: "img/heart.svg",
-    },
-    {
-      name: "heart",
-      img: "img/heart.svg",
-    },
-  ];
+const cardArray = [
+  {
+    name: "controller",
+    img: "img/controller.svg",
+  },
+  {
+    name: "controller",
+    img: "img/controller.svg",
+  },
+  {
+    name: "gift",
+    img: "img/gift.svg",
+  },
+  {
+    name: "gift",
+    img: "img/gift.svg",
+  },
+  {
+    name: "key",
+    img: "img/key.svg",
+  },
+  {
+    name: "key",
+    img: "img/key.svg",
+  },
+  {
+    name: "location",
+    img: "img/location.svg",
+  },
+  {
+    name: "location",
+    img: "img/location.svg",
+  },
+  {
+    name: "magnet",
+    img: "img/magnet.svg",
+  },
+  {
+    name: "magnet",
+    img: "img/magnet.svg",
+  },
+  {
+    name: "seat",
+    img: "img/seat.svg",
+  },
+  {
+    name: "seat",
+    img: "img/seat.svg",
+  },
+  {
+    name: "sun",
+    img: "img/sun.svg",
+  },
+  {
+    name: "sun",
+    img: "img/sun.svg",
+  },
+  {
+    name: "wifi",
+    img: "img/wifi.svg",
+  },
+  {
+    name: "wifi",
+    img: "img/wifi.svg",
+  },
+  {
+    name: "heart",
+    img: "img/heart.svg",
+  },
+  {
+    name: "heart",
+    img: "img/heart.svg",
+  },
+  {
+    name: "phone",
+    img: "img/phone.svg",
+  },
+  {
+    name: "phone",
+    img: "img/phone.svg",
+  },
+];
 
-  // const easeMode = cardArray.length - 12;
-  // const normalMode = cardArray.length - 8;
-  // const hardMode = cardArray.length - 4;
-  // const extremeMode = cardArray.length;
-  // const mode;
-  cardArray.sort(() => 0.5 - Math.random());
-
-  const grid = document.querySelector(".memory-game");
-  const resultDisplay = document.getElementById("result");
-  let cardsChosen = [];
-  let cardsChosenId = [];
-  let cardsWon = [];
-
-  function createBoard() {
-    for (let i = 0; i < cardArray.length; i++) {
-      let card = document.createElement("div");
-      let img = document.createElement("img");
-
-      card.setAttribute("class", "card");
-      img.setAttribute("src", "img/blank.svg");
-      img.setAttribute("data-id", i);
-
-      card.appendChild(img);
-
-      card.addEventListener("click", (e) => {
-        flipCard(e);
-      });
-
-      grid.appendChild(card);
+let radios = document.querySelectorAll('input[type="radio"]');
+let mode;
+let arrayMode = [];
+let time;
+radios.forEach((radio) => {
+  radio.addEventListener("change", () => {
+    mode = radio.value;
+    switch (mode) {
+      case "ease":
+        const easeMode = cardArray.length - 12;
+        for (let i = 0; i < easeMode; i++) {
+          arrayMode.push(cardArray[i]);
+        }
+        time = 400;
+        break;
+      case "normal":
+        const normalMode = cardArray.length - 8;
+        for (let i = 0; i < normalMode; i++) {
+          arrayMode.push(cardArray[i]);
+        }
+        time = 300;
+        break;
+      case "hard":
+        const hardMode = cardArray.length - 4;
+        for (let i = 0; i < hardMode; i++) {
+          arrayMode.push(cardArray[i]);
+        }
+        time = 200;
+        break;
+      case "extreme":
+        const extremeMode = cardArray.length;
+        for (let i = 0; i < extremeMode; i++) {
+          arrayMode.push(cardArray[i]);
+        }
+        time = 100;
+        break;
+      default:
+        return;
     }
-  }
-  function checkForMatch(e) {
-    let cards = document.querySelectorAll("img");
-    const optionOneId = cardsChosenId[0];
-    const optionTwoId = cardsChosenId[1];
-    if (cardsChosen[0] === cardsChosen[1]) {
-      cards[optionOneId].setAttribute("src", "img/done.svg");
-      cards[optionTwoId].setAttribute("src", "img/done.svg");
-      cards[optionOneId].removeEventListener("click", flipCard);
-      cards[optionTwoId].removeEventListener("click", flipCard);
-      cardsWon.push(cardsChosen);
-    }
-    if (cardsChosen[0] !== cardsChosen[1]) {
-      cards[optionOneId].setAttribute("src", "img/blank.svg");
-      cards[optionTwoId].setAttribute("src", "img/blank.svg");
-      cards[optionOneId].parentNode.style.background = "var(--linearBack)";
-      cards[optionTwoId].parentNode.style.background = "var(--linearBack)";
-    }
-    cardsChosen = [];
-    cardsChosenId = [];
 
-    resultDisplay.textContent = cardsWon.length;
-    if (cardsWon.length === cardArray.length / 2) {
-      resultDisplay.parentNode.textContent =
-        "Congratulations! You found them All";
-    }
-  }
-
-  function flipCard(e) {
-    let cardId = e.target.getAttribute("data-id");
-    cardsChosen.push(cardArray[cardId].name);
-    cardsChosenId.push(cardId);
-    e.target.setAttribute("src", cardArray[cardId].img);
-    e.target.parentNode.style.background = "var(--linearFront)";
-    if (cardsChosen.length === 2) {
-      setTimeout(() => {
-        checkForMatch(e);
-      }, 300);
-    }
-  }
-
-  createBoard();
+    createBoard();
+    radio.parentNode.parentNode.style.display = "none";
+  });
 });
+
+const grid = document.querySelector(".memory-game");
+const resultDisplay = document.getElementById("result");
+let cardsChosen = [];
+let cardsChosenId = [];
+let cardsWon = [];
+
+arrayMode = arrayMode.sort(() => (0.5 - Math.random()));
+
+function createBoard() {
+  console.log(arrayMode);
+  for (let i = 0; i < arrayMode.length; i++) {
+    let card = document.createElement("div");
+    let img = document.createElement("img");
+
+    card.setAttribute("class", "card");
+    img.setAttribute("src", "img/blank.svg");
+    img.setAttribute("data-id", i);
+
+    card.appendChild(img);
+
+    card.addEventListener("click", (e) => {
+      flipCard(e);
+    });
+
+    grid.appendChild(card);
+  }
+}
+function checkForMatch() {
+  let cards = document.querySelectorAll("img");
+  const optionOneId = cardsChosenId[0];
+  const optionTwoId = cardsChosenId[1];
+  if (cardsChosen[0] === cardsChosen[1]) {
+    cards[optionOneId].setAttribute("src", "img/done.svg");
+    cards[optionTwoId].setAttribute("src", "img/done.svg");
+    cards[optionOneId].removeEventListener("click", flipCard);
+    cards[optionTwoId].removeEventListener("click", flipCard);
+    cardsWon.push(cardsChosen);
+  }
+  if (cardsChosen[0] !== cardsChosen[1]) {
+    cards[optionOneId].setAttribute("src", "img/blank.svg");
+    cards[optionTwoId].setAttribute("src", "img/blank.svg");
+    cards[optionOneId].parentNode.style.background = "var(--linearBack)";
+    cards[optionTwoId].parentNode.style.background = "var(--linearBack)";
+  }
+  cardsChosen = [];
+  cardsChosenId = [];
+
+  resultDisplay.textContent = cardsWon.length;
+  if (cardsWon.length === arrayMode.length / 2) {
+    resultDisplay.parentNode.textContent =
+      "Congratulations! You found them All";
+  }
+}
+
+function flipCard(e) {
+  let cardId = e.target.getAttribute("data-id");
+  cardsChosen.push(arrayMode[cardId].name);
+  cardsChosenId.push(cardId);
+  e.target.setAttribute("src", arrayMode[cardId].img);
+  e.target.parentNode.style.background = "var(--linearFront)";
+  if (cardsChosen.length === 2) {
+    setTimeout(() => {
+      checkForMatch(e);
+    }, time);
+  }
+}
---
+++ day31to40/social-proof/README.md
@@ -0,0 +1,40 @@
+# Frontend Mentor - Social proof section solution
+
+This is a solution to the [Social proof section challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/social-proof-section-6e0qTv_bA). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [My process](#my-process)
+- [Built with](#built-with)
+- [Author](#author)
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the section depending on their device's screen size
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://sones-100days.netlify.app/day31to40/social-proof)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+
+## Author
+
+- Linkedin - [Scarabelli](https://www.linkedin.com/in/scarabelli/)
+- Frontend Mentor - [@S0NES](https://www.frontendmentor.io/profile/S0NES)
+- Twitter - [@S0Soneca](https://www.twitter.com/S0Soneca)
+- Github - [S0NES](https://github.com/S0NES)
+
+## 32/100 4/08/2021 - DONE
+
+## #100DAYSCHALLENGE
