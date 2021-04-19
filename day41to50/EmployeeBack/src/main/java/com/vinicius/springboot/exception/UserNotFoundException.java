d13658f806e2a6b7664c9d2703998cc4c9c725e4
S0NES <scarabelli.vinicius@gmail.com>
2021-04-18 21:38:28 -0300

    correction day 41 an finalized day 41

--- day41to50/EmployeeBack/src/main/java/com/vinicius/springboot/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.vinicius.springboot.exception;
-
-
-public class UserNotFoundException extends RuntimeException{
-	public UserNotFoundException(String message) {
-		super(message);
-	}
-}
