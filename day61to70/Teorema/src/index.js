4567644e456916b8e2fbfeea110996cba2b1ec04
S0NES <scarabelli.vinicius@gmail.com>
2021-05-07 20:55:51 -0300

    feature

--- day61to70/Teorema/src/index.js
+++ day61to70/Teorema/src/index.js
@@ -21,33 +21,49 @@ btn.addEventListener("click", () => {
   showResult(a, b, c);
 });
 function showResult(a, b, c) {
-  let sqrtA = Math.pow(a, 2);
-  let sqrtB = Math.pow(b, 2);
-  setTimeout(() => {
-    result.innerHTML = `c² = ${a}² + ${b}² <br/>`;
-    valueC.innerText = 'C';
-    audio.play();
-  }, 500);
-  setTimeout(() => {
-    result.innerHTML += `c² = ${sqrtA} + ${sqrtB} <br/>`;
-    audio.play();
-  }, 1000);
-  setTimeout(() => {
-    result.innerHTML += `c² = ${sqrtA + sqrtB} <br/>`;
-    audio.play();
-  }, 1500);
-  setTimeout(() => {
-    result.innerHTML += `c = &radic;${c}<br/>`;
-    audio.play();
-  }, 2000);
-  setTimeout(() => {
-    result.innerHTML += `c = ${c}`;
-    audio.play();
-    valueC.innerText = c;
-  }, 2500);
+  if (
+    a === undefined ||
+    b === undefined ||
+    a === null ||
+    b === null ||
+    a === "" ||
+    b === ""
+  ) {
+    result.innerHTML = "PLEASE DIGIT SOMETHING";
+    return;
+  } else {
+    print(a, b, c);
+  }
 }
 function calculate(a, b) {
   let c;
   c = Math.floor(Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)));
   return c;
+}
+
+function print(a, b, c) {
+  let sqrtA = Math.pow(a, 2);
+  let sqrtB = Math.pow(b, 2);
+  setTimeout(() => {
+    result.innerHTML = `c² = ${a}² + ${b}² <br/>`;
+    valueC.innerText = "C";
+    audio.play();
+  }, 100);
+  setTimeout(() => {
+    result.innerHTML += `c² = ${sqrtA} + ${sqrtB} <br/>`;
+    audio.play();
+  }, 600);
+  setTimeout(() => {
+    result.innerHTML += `c² = ${sqrtA + sqrtB} <br/>`;
+    audio.play();
+  }, 1100);
+  setTimeout(() => {
+    result.innerHTML += `c = &radic;${sqrtA + sqrtB}<br/>`;
+    audio.play();
+  }, 1600);
+  setTimeout(() => {
+    result.innerHTML += `c = ${c}`;
+    audio.play();
+    valueC.innerText = c;
+  }, 2100);
 }
